aacdbae560984195b34de435d04a82117383447e
Agent Ebimene <ebimeneagent@gmail.com>
2022-09-29 12:59:16 +0100

    Seperate Logic and routes with controllers

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,18 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
-
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/users";
 const router = express.Router();
-let users = [];
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/", getUsers);
 
-router.post("/", (req, res) => {
-  const user = req.body;
-  users.push({ ...user, id: uuidv4() });
-  res.send(`user with the name ${user.firstName} added to the database`);
-});
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const foundUser = users.find((user) => user.id === id);
-  res.send(foundUser);
-});
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  users = users.filter((user) => user.id !== id);
-  res.send(`User with the Id ${id} deleted from database`);
-});
+router.post("/", createUser);
+router.get("/:id", getUser);
+router.delete("/:id", deleteUser);
+
+router.patch("/:id", updateUser);
 export default router;
